c0bd1e853af1eccea400a9422d65a603824ddef1
Tyuiu.KosovAS@outlook.com <andrey.kosov.05@inbox.ru>
2023-10-08 12:06:40 +0500

    Task7

---
+++ Tyuiu.KosovAS.Sprint2.Task7.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KosovAS.Sprint2.Task7.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KosovAS.Sprint2.Task7.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("48cf64e2-0b64-4206-9f00-82b59d375765")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KosovAS.Sprint2.Task7.V14/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KosovAS.Sprint2.Task7.V14.Lib;
+
+namespace Tyuiu.KosovAS.Sprint2.Task7.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            double x; double y;
+
+            Console.Title = "Спринт #1 | Выполнил: Косов А. С. | АСОиУБ-23-3";
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* Спринт #2" + String.Concat(Enumerable.Repeat(" ", 63)) + "*");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Вариант 14" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine("* Выполнил: Косов Андрей Сергеевич | АСОиУБ-23-3" + String.Concat(Enumerable.Repeat(" ", 26)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* УСЛОВИЕ:" + String.Concat(Enumerable.Repeat(" ", 64)) + "*");
+            Console.WriteLine("* Написать програму, которая запрашивает у пользователя данные, вычисляет *");
+            Console.WriteLine("* их и печатает их на экран.                                              *");
+            Console.WriteLine("*" + String.Concat(Enumerable.Repeat(" ", 73)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:" + String.Concat(Enumerable.Repeat(" ", 56)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+            Console.WriteLine("Введите значение переменной X: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+            Console.WriteLine("* Результат:" + String.Concat(Enumerable.Repeat(" ", 62)) + "*");
+            Console.WriteLine(String.Concat(Enumerable.Repeat("*", 75)));
+
+            if (ds.CheckDotInShadedArea(x, y) == true)
+            {
+                Console.WriteLine($"Точка ({x}, {y}) находиться в заданной области");
+            }
+            else { Console.WriteLine($"Точка ({x}, {y}) не находиться в заданной области"); }
+
+            Console.ReadKey();
+        }
+    }
+}
